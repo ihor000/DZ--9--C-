609889d902f257c863db4ab445d94c6da87a30e5
ihor000 <ihor.silnov@yandex.ru>
2023-01-10 22:41:39 +0300

    text

---
+++ 1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите натуральное число больше 1");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NumberCounter (int number)
+{
+    if (number < 0) Console.Write($"{number} не натуральное число");
+    if (number == 0) return;
+    Console.Write("{0,4}", number);
+    NumberCounter (number - 1);
+}
+
+NumberCounter(number);
