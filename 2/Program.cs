f7d748c116c84d9556c1279cc5175e0a8d0bef1d
ihor000 <ihor.silnov@yandex.ru>
2023-01-10 22:58:39 +0300

    text

---
+++ 2/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите начальное число M");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите начальное число N");
+int numberN =Convert.ToInt32 (Console.ReadLine());
+
+void GapNumberSum (int numberM, int numberN, int sum)
+{
+    if (numberM > numberN) 
+    {
+        Console.WriteLine($"Сумма элементов в промежутке от M до N: {sum}"); 
+        return;
+    }
+    sum = sum + (numberM++);
+    GapNumberSum(numberM, numberN, sum);
+}
+
+GapNumberSum(numberM, numberN, 0);
